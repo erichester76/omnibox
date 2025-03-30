839a37180a386bd97d4bc77c36539a078b145864
Eric Hester <eric.hester@umbrella.tech>
2025-03-30 18:19:24 -0400

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
           <main className="min-h-screen flex flex-col">
             {/* Header */}
             <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-              <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+              <div className="w-full flex justify-between items-center p-3 px-5 text-sm">
                 <div className="flex gap-5 items-center font-semibold">
                   <Link href={"/"}>OMNIbox</Link>
                 </div>
