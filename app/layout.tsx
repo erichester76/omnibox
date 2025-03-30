8e8c7b48a17766d599c6da05eb02cb0c2d0dd98f
Eric Hester <eric.hester@umbrella.tech>
2025-03-30 18:30:05 -0400

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,10 +38,8 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <main className="min-h-screen flex flex-col">
-            {/* Header */}
-            
             <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-              <div className="w-full flex justify-between items-center p-3 px-5 text-sm">
+              <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                 <div className="flex gap-5 items-center font-semibold">
                   <Link href={"/"}>OMNIbox</Link>
                 </div>
@@ -49,16 +47,11 @@ export default function RootLayout({
                 <ThemeSwitcher />
               </div>
             </nav>
-
-            {/* Main Content Area */}
-            <div className="flex-1 w-full flex flex-col items-center">
+            <div className="flex-1 w-full">
               {children}
             </div>
-            
-            {/* Footer */}
             <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
             </footer>
-          
           </main>
         </ThemeProvider>
       </body>
